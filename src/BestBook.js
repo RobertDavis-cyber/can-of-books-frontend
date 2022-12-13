7540ad461070950ffb4454851200d67144eb9cf0
Robert Davis <robertcd@hawaii.edu>
2022-12-12 19:22:37 -1000

    lab12 setup

--- src/BestBook.js
+++ src/BestBook.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import axios from 'axios';
-import Carousel from 'react-bootstrap/Carousel';
+// import Carousel from 'react-bootstrap/Carousel';
 
 class BestBooks extends React.Component {
     constructor() {
@@ -23,23 +23,23 @@ class BestBooks extends React.Component {
 
 
     componentDidMount = () => {
-        this.fetchBooks();
+        this.bookData();
     }
 
-    fetchBooks = async (bookName) => {
-        let request = {
-            method: 'GET',
-            url: 'http://localhost:3001/books'
-        }
-        if (bookName) {
-            request.url += `?name=${bookName}`
-        }
-        console.log(request);
-        let response = await axios(request);
-        this.setState({
-            books: response.data,
-        });
-    }
+    // fetchBooks = async (bookName) => {
+    //     let request = {
+    //         method: 'GET',
+    //         url: 'http://localhost:3001/books'
+    //     }
+    //     if (bookName) {
+    //         request.url += `?name=${bookName}`
+    //     }
+    //     console.log(request);
+    //     let response = await axios(request);
+    //     this.setState({
+    //         books: response.data,
+    //     });
+    // }
 
     handleChange = (e) => {
         console.log('hand change running', e.traget.value)
@@ -47,52 +47,10 @@ class BestBooks extends React.Component {
     }
 
     render() {
+        console.log(this.state)
 
         return (
-
-            <Carousel className="carousel">
-                {this.state.books.map((book) => {
-                    <Carousel.Item key={book._id}>
-                        <img
-                            src="https://images.unsplash.com/photo-1535905557558-afc4877a26fc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"
-                            alt="First slide"
-                        />
-                        <Carousel.Caption>
-                            <h3>{book.title}</h3>
-                            <p>{book.description}</p>
-                            <h2>{book.status}</h2>
-                        </Carousel.Caption>
-
-                    </Carousel.Item>
-                }
-                {this.state.books.length > 0 ? (
-                <Carousel>
-                    {booksDisplay}
-                </Carousel>
-        ) : ( 
-            <h3>No Books found!</h3>
-
-
-                
-            {/* ) 
-            }
-
-
-                    < div id = "books-component" >
-                <input onChange={this.handleChange} type="text"/>
-                <button onClick={() => this.fetchBooks(this.state.search)}>Search for Book</button>
-                {
-                        this.state.books.map(book => {
-                            return (
-                                <div>
-                                    <h2>{book.title}</h2>
-                                    <p>{book.description}</p>
-                                    <p>{book.status}</p>
-                                </div>
-                            )
-                        })
-                    } */}
-            </div >
+           <h2>These are the best books</h2>
         )
     }
 }
