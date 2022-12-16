9cbb7e13c06f368389ba128beb59a0e7ad483831
Robert Davis <robertcd@hawaii.edu>
2022-12-16 10:17:31 -1000

    setup autho0 front

--- src/index.js
+++ src/index.js
@@ -3,11 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Auth0Provider } from '@auth0/auth0-react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider
+      domain='dev-7z0vul1oq3p1ry26.us.auth0.com'
+      clientID='HsvSkXGE0ZCJus0CGO2x9IZcFZWfXriv'
+      redirectUrl='http://localhost:3000'
+
+    >
+      <App />
+    </Auth0Provider>
   </React.StrictMode>
 );
 
